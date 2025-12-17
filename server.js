db38912e6e85a28083d9722a92765e9007a5275a
indra priyadharshini v <indrapriyadharshini.v2024it.sece.ac.in>
2025-12-17 12:28:42 +0530

    Initial MERN project setup

--- server.js
+++ server.js
@@ -16,4 +16,4 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/students",studentRoutes);
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=>console.log('Server running on port ${PORT}'));
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
